cba8dc54fe39a7988b407367100e396085fa9f73
ppoitier <pierre.poitier@unamur.be>
2025-12-16 14:38:05 +0100

    Add sign language instance entity

---
+++ sldp/entities/sign-language-sample.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+import numpy as np
+import pandas as pd
+from pydantic import BaseModel
+
+
+class SignLanguageSample(BaseModel):
+    id: str
+    signer_id: str
+    sign_language: str
+    dataset: Optional[str] = None
+
+    label: Optional[str] = None
+    label_id: Optional[int] = None
+
+    poses: Optional[dict[str, np.ndarray]] = None
+    video: Optional[bytes] = None
+    video_path: Optional[str] = None
+
+    annotations: Optional[pd.DataFrame] = None
